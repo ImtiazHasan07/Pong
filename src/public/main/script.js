8378505046898925147c5d6a0fc5a3efc4270dbb
ImtiazHasan07 <imtiazhasan_07@outlook.com>
2026-06-17 03:14:55 +0100

    a

--- src/public/main/script.js
+++ src/public/main/script.js
@@ -1,6 +1,8 @@
 const player1Score = document.getElementById('player-1-score')
 const player2Score = document.getElementById('player-2-score')
 
+let gameStarting = true
+
 player1Score.innerText = (0).toLocaleString('en-US', {
     minimumIntegerDigits: 2,
     useGrouping: false
@@ -31,12 +33,45 @@ class Game {
 }
 
 function main() {
+    let gameContainer = document.getElementById('game-container')
     let game = new Game(750, 750);
-}
 
-const gameContainer = document.getElementById('game_container')
-gameContainer.addEventListener('contextmenu', (event) => {
-    event.preventDefault()
-});
+    document.addEventListener('keydown', (event) => {
+        let key = event.key
+        // console.log(event)
+    
+        if (gameStarting) {
+            let startPrompt = document.getElementById('start-prompt')
+            startPrompt.hidden = true
+    
+            gameStarting = false
+        } else {
+            let player1 = document.getElementById('player-1')
+            let player2 = document.getElementById('player-2')
+    
+    
+            if (key === 'w') {
+                console.log('hi')
+                if (Number(getComputedStyle(player1)['top'].slice(0, -2)) <= 80) return;
+                console.log('hi2')
+                player1.style['top'] = `${Number(getComputedStyle(player1)['top'].slice(0, -2)) - 20 }px`
+            } else if (key === 's') {
+                if (Number(getComputedStyle(player1)['top'].slice(0, -2)) >= 760) return;
+                player1.style['top'] = `${Number(getComputedStyle(player1)['top'].slice(0, -2)) + 20 }px`
+            } else if (key === 'ArrowUp') {
+                if (Number(getComputedStyle(player2)['top'].slice(0, -2)) <= 80) return;
+                player2.style['top'] = `${Number(getComputedStyle(player2)['top'].slice(0, -2)) - 20 }px`
+            } else if (key === 'ArrowDown') {
+                if (Number(getComputedStyle(player2)['top'].slice(0, -2)) >= 760) return;
+                player2.style['top'] = `${Number(getComputedStyle(player2)['top'].slice(0, -2)) + 20 }px`
+            }
+            console.log(`Player 1: ${getComputedStyle(player1)['top']}`)
+            console.log(`Player 2: ${getComputedStyle(player2)['top']}`)
+        }
+    })
+    gameContainer.addEventListener('contextmenu', (event) => {
+        event.preventDefault()
+    });
+}
 
 main()
