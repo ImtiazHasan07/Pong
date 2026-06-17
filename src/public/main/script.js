2e84977331a72adf87af0f3fdc7a3f7e46e1703c
ImtiazHasan07 <imtiazhasan_07@outlook.com>
2026-06-17 03:14:57 +0100

    a

--- src/public/main/script.js
+++ src/public/main/script.js
@@ -3,43 +3,56 @@ const context = canvas.getContext('2d');
 
 class Game {
   constructor() {
-    // this.scoreLimit = 10
-    this.scoreLimit = 3
+    this.scoreLimit = 10
     this.gameInProgress = false;
     
     this.paddleHeight = 150;
     this.paddleWidth = 15;
-    this.paddleSpeed = 10;
+    this.step = 10;
 
     this.paddle1Data = {
       score: 0,
-      x: 10,
-      y: canvas.height / 2 - 75,
+      originalX: 10,
+      originalY: canvas.height / 2 - 75,
+      x: null,
+      y: null,
       width: null,
       height: null,
+      step: null
     };
+    this.paddle1Data.x = this.paddle1Data.originalX;
+    this.paddle1Data.y = this.paddle1Data.originalY;
     this.paddle1Data.width = this.paddleWidth;
     this.paddle1Data.height = this.paddleHeight;
+    this.paddle1Data.step = this.step;
 
     this.paddle2Data = {
       score: 0,
-      x: canvas.width - 20,
-      y: canvas.height / 2 - 75,
+      originalX: canvas.width - 25,
+      originalY: canvas.height / 2 - 75,
+      x: null,
+      y: null,
       width: null,
       height: null,
+      step: null
     };
+    this.paddle2Data.x = this.paddle2Data.originalX;
+    this.paddle2Data.y = this.paddle2Data.originalY;
     this.paddle2Data.width = this.paddleWidth;
     this.paddle2Data.height = this.paddleHeight;
+    this.paddle2Data.step = this.step;
 
     this.ball = {
         originalX: canvas.width / 2 - 20,
         originalY: canvas.height / 2 - 30,
+        originalStep: 5,
         originalSpeed: 5,
         originalDirection: -180,
         x: null,
         y: null,
         width: 10,
         height: 10,
+        step: 5,
         speed: null,
         direction: null,
       };
@@ -56,28 +69,6 @@ class Game {
       keys[key] = true;
 
       if (!this.gameInProgress) {
-        function resetGame(game) {
-          game.paddle1Data = {
-            score: 0,
-            x: 10,
-            y: canvas.height / 2 - 75,
-            width: null,
-            height: null,
-          };
-          game.paddle1Data.width = game.paddleWidth;
-          game.paddle1Data.height = game.paddleHeight;
-
-          game.paddle2Data = {
-            score: 0,
-            x: canvas.width - 20,
-            y: canvas.height / 2 - 75,
-            width: null,
-            height: null,
-          };
-          game.paddle2Data.width = game.paddleWidth;
-          game.paddle2Data.height = game.paddleHeight;
-        }
-        resetGame(this);
         this.gameInProgress = true;
       }
     });
@@ -92,36 +83,49 @@ class Game {
     });
 
     setInterval(() => {
-      //   window.requestAnimationFrame(this.draw);
       this.draw();
+
       if (this.gameInProgress) {
+        if ([this.paddle1Data.score, this.paddle2Data.score].some((value) => value >= this.scoreLimit)) {
+          this.ball.x = -500;
+          context.font = '50px Arial';
+          let text = `Player ${this.paddle1Data.score >= this.scoreLimit ? 1 : 2} won!`
+          let textWidth = context.measureText(text).width;
+          context.fillText(text, (canvas.width - textWidth) / 2, canvas.height / 2);
+          // this.resetPaddles(); // might be unnecessary
+          setTimeout(() => {
+            this.gameInProgress = false;
+            this.newGame();
+          }, 3000);
+          return;
+        }
         if (keys['w']) {
           if (this.paddle1Data.y >= 0) {
-            this.paddle1Data.y -= this.paddleSpeed;
+            this.paddle1Data.y -= this.step;
           };
         }
         if (keys['s']) {
             if (this.paddle1Data.y <= canvas.height - this.paddleHeight) {
-            this.paddle1Data.y += this.paddleSpeed;
+            this.paddle1Data.y += this.step;
           };
         }
         if (keys['ArrowUp']) {
           if (this.paddle2Data.y >= 0) {
-            this.paddle2Data.y -= this.paddleSpeed;
+            this.paddle2Data.y -= this.step;
           }
         }
         if (keys['ArrowDown']) {
             if (this.paddle2Data.y <= canvas.height - this.paddleHeight) {
-            this.paddle2Data.y += this.paddleSpeed;
+            this.paddle2Data.y += this.step;
           };
         }
 
         if (this.ball.direction < 0) {
-          this.ball.x += Math.cos(degreesToRadians(this.ball.direction)) * this.ball.speed;
-          this.ball.y += Math.sin(degreesToRadians(this.ball.direction)) * this.ball.speed;
+          this.ball.x += Math.cos(degreesToRadians(this.ball.direction)) * this.ball.step;
+          this.ball.y += Math.sin(degreesToRadians(this.ball.direction)) * this.ball.step;
         } else {
-          this.ball.x -= Math.cos(degreesToRadians(this.ball.direction)) * this.ball.speed;
-          this.ball.y -= Math.sin(degreesToRadians(this.ball.direction)) * this.ball.speed;
+          this.ball.x -= Math.cos(degreesToRadians(this.ball.direction)) * this.ball.step;
+          this.ball.y -= Math.sin(degreesToRadians(this.ball.direction)) * this.ball.step;
         }
 
         if (this.ball.y <= 0 || this.ball.y >= canvas.height) {
@@ -130,7 +134,7 @@ class Game {
           } else {
             this.ball.direction -= 90;
           }
-          this.ball.speed += 1;
+          this.ball.step += 1;
         }
 
         if (this.ball.x <= this.paddle1Data.x + this.paddle1Data.width && this.ball.y >= this.paddle1Data.y && this.ball.y <= this.paddle1Data.y + this.paddle1Data.height) {
@@ -140,7 +144,7 @@ class Game {
           } else {
             this.ball.direction -= 45;
           }
-          this.ball.speed += 1;
+          this.ball.step += 1;
         }
 
         if (this.ball.x >= this.paddle2Data.x && this.ball.y >= this.paddle2Data.y && this.ball.y <= this.paddle2Data.y + this.paddle2Data.height) {
@@ -150,59 +154,62 @@ class Game {
           } else {
             this.ball.direction -= 45;
           }
-          this.ball.speed += 1;
+          this.ball.step += 1;
         }
 
         if (this.ball.x <= 0 || this.ball.x >= canvas.width) {
-          function resetBall(ball) {
+          function resetBall(ball, roundWinner) {
             ball.x = ball.originalX;
             ball.y = ball.originalY;
-            ball.x = ball.originalX;
-            ball.y = ball.originalY;
+            ball.step = ball.originalStep;
             ball.speed = ball.originalSpeed;
-            ball.direction = ball.originalDirection;
+            if (roundWinner === 'Player 2') {
+              ball.direction = ball.originalDirection;
+            } else {
+              ball.direction = -ball.originalDirection;
+            }
           }
-
-          console.log(1, this.ball.x <= 0, this.ball.x >= canvas.width, this.paddle1Data, this.paddle2Data);
+          let roundWinner = null
           if (this.ball.x <= 0) {
             this.paddle2Data.score += 1;
+            roundWinner = 'Player 2'
           } else {
             this.paddle1Data.score += 1;
+            roundWinner = 'Player 1'
           }
-          console.log(this.ball.x <= 0, this.ball.x >= canvas.width, this.paddle1Data, this.paddle2Data);
-          resetBall(this.ball);
+          resetBall(this.ball, roundWinner);
         }
 
-        if ([this.paddle1Data.score, this.paddle2Data.score].some((value) => value >= this.scoreLimit)) {
-          this.ball.x = -500;
-          context.font = '50px Arial';
-          context.fillText(`Player ${this.paddle1Data.score >= this.scoreLimit ? 1 : 2} won!`, 125, canvas.height / 2);
-          setTimeout(() => {
-            console.log(1)
-            this.gameInProgress = false;
-          });
-        }
+
       }
     }, 1);
   }
 
+  resetPaddles() { // might be unnecessary
+    this.paddle1Data.x = this.paddle1Data.originalX
+    this.paddle2Data.x = this.paddle2Data.originalX
+    this.paddle1Data.y = this.paddle1Data.originalY
+    this.paddle2Data.y = this.paddle2Data.originalY
+  }
+
+  newGame() {
+    this.paddle1Data.score = 0
+    this.paddle2Data.score = 0
+    this.paddle1Data.x = this.paddle1Data.originalX
+    this.paddle1Data.x = this.paddle1Data.originalX
+    this.paddle1Data.y = this.paddle1Data.originalY
+    this.paddle1Data.y = this.paddle1Data.originalY
+    this.paddle1Data.width = this.paddleWidth;
+    this.paddle2Data.width = this.paddleWidth;
+    this.paddle1Data.height = this.paddleHeight;
+    this.paddle2Data.height = this.paddleHeight;
+  }
+
   draw() {
     function drawCanvas() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
       context.fillStyle = '#000000';
+      context.clearRect(0, 0, canvas.width, canvas.height);
       context.fillRect(0, 0, canvas.width, canvas.height);
-    }
-
-    function drawPlayerScores(paddle1Data, paddle2Data) {
-      context.fillStyle = '#ffffff';
-      context.fillRect(paddle1Data.x, paddle1Data.y, paddle1Data.width, paddle1Data.height);
-      context.font = '100px Arial';
-      context.fillText(`${formatNumber(paddle1Data.score)}`, 50, 100);
-
-      context.fillStyle = '#ffffff';
-      context.fillRect(paddle2Data.x, paddle2Data.y, paddle2Data.width, paddle2Data.height);
-      context.font = '100px Arial';
-      context.fillText(`${formatNumber(paddle2Data.score)}`, canvas.width - 150, 100);
     }
 
     function drawDashedLine() {
@@ -214,10 +221,26 @@ class Game {
       context.strokeStyle = '#ffffff';
       context.stroke();
     }
+    
+    function drawPlayerScores(paddle1Data, paddle2Data) {
+      context.fillStyle = '#ffffff';
+      context.font = '100px Arial';
+      context.fillText(`${formatNumber(paddle1Data.score)}`, 50, 100);
+      context.fillText(`${formatNumber(paddle2Data.score)}`, canvas.width - 150, 100);
+    }
+
+    function drawPaddles(paddle1Data, paddle2Data) {
+      context.fillStyle = '#ffffff';
+      context.fillRect(paddle1Data.x, paddle1Data.y, paddle1Data.width, paddle1Data.height);
+      context.fillRect(paddle2Data.x, paddle2Data.y, paddle2Data.width, paddle2Data.height);
+    }
 
     function drawStartingMessage() {
+      context.fillStyle = '#ffffff';
       context.font = '50px Arial';
-      context.fillText('Press any key to begin', 125, canvas.height / 2);
+      let text = 'Press any key to begin'
+      let textWidth = context.measureText(text).width;
+      context.fillText(text, (canvas.width - textWidth) / 2, canvas.height / 2);
     }
 
     function drawBall(x, y, width, height) {
@@ -226,8 +249,9 @@ class Game {
     }
 
     drawCanvas();
-    drawPlayerScores(this.paddle1Data, this.paddle2Data);
     drawDashedLine();
+    drawPlayerScores(this.paddle1Data, this.paddle2Data);
+    drawPaddles(this.paddle1Data, this.paddle2Data)
 
     if (!this.gameInProgress) {
       drawStartingMessage();
@@ -275,6 +299,10 @@ function radiansToDegrees(radians) {
 
 function getDifference(a, b) {
   return a > b ? a - b : b - a;
+}
+
+function getVariableName(variable) {
+    return Object.keys({ variable })[0]
 }
 
 let game = new Game();
