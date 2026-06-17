30e6a30546ad06423ab069229e2c56e0fd8901ee
ImtiazHasan07 <imtiazhasan_07@outlook.com>
2026-06-17 03:14:57 +0100

    Added Classes
    Add classes, lots of issues that need to be fixed

--- src/public/main/script.js
+++ src/public/main/script.js
@@ -1,78 +1,37 @@
-const canvas = document.getElementById('game')
-const context = canvas.getContext('2d')
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
 
-function angleCalculator(x1, y1, x2, y2) {
-    console.log(`(${x1}, ${y1}), (${x2}, ${y2})`)
-    // let differenceInX;
-    // let differenceInY;
-    // if (x1 > x2 || y1 > y2) {
-    //     differenceInX = x1 - x2
-    //     differenceInY = y1 - y2
-    // } else {
-    //     differenceInX = x2 - x1
-    //     differenceInY = y2 - y1
-    // }
-    let differenceInX = getDifference(x1, x2);
-    let differenceInY = getDifference(y1, y2);
+class Game {
+  constructor() {
+    // this.scoreLimit = 10
+    this.scoreLimit = 3
+    this.gameInProgress = false;
+    
+    this.paddleHeight = 100;
+    this.paddleWidth = 10;
+    this.paddleSpeed = 10;
 
-    let opposite = differenceInY;
-    let adjacent = differenceInX;
+    this.paddle1Data = {
+      score: 0,
+      x: 10,
+      y: canvas.height / 2 - 75,
+      width: null,
+      height: null,
+    };
+    this.paddle1Data.width = this.paddleWidth;
+    this.paddle1Data.height = this.paddleHeight;
 
-    console.log(opposite)
-    console.log(adjacent)
-    console.log(opposite / adjacent)
-    let radianAngle = Math.atan(degreesToRadians(opposite / adjacent))
-    let degreesAngle = radiansToDegrees(radianAngle)
+    this.paddle2Data = {
+      score: 0,
+      x: canvas.width - 20,
+      y: canvas.height / 2 - 75,
+      width: null,
+      height: null,
+    };
+    this.paddle2Data.width = this.paddleWidth;
+    this.paddle2Data.height = this.paddleHeight;
 
-    return degreesAngle
-
-    // if (!angle) return 180
-    // return angle
-}
-
-function formatNumber(number) {
-    return number.toLocaleString('en-US', {
-        minimumIntegerDigits: 2,
-        useGrouping: false
-    })
-}
-
-function degreesToRadians(degrees) {
-    return degrees * Math.PI / 180;
-}
-
-function radiansToDegrees(radians) {
-    return radians * (180 / Math.PI);
-}
-
-function getDifference(a, b) {
-    return a > b ? a - b : b - a
-}
-
-
-function main() {
-    let gameStarting = true
-
-    let paddleWidth = 10
-    let paddleHeight = 100
-
-    let player_1 = {
-        score: 0,
-        x: 10,
-        y: canvas.height / 2 - 75,
-        width: paddleWidth,
-        height: paddleHeight
-    }
-
-    let player_2 = {
-        score: 0,
-        x: canvas.width - 20,
-        y: canvas.height / 2 - 75,
-        width: paddleWidth,
-        height: paddleHeight
-    }
-
-    let ball = {
+    this.ball = {
         originalX: canvas.width / 2 - 20,
         originalY: canvas.height / 2 - 30,
         originalSpeed: 5,
@@ -82,160 +41,239 @@ function main() {
         width: 10,
         height: 10,
         speed: null,
-        direction: null
-    }
-    ball.x = ball.originalX
-    ball.y = ball.originalY
-    ball.speed = ball.originalSpeed
-    ball.direction = ball.originalDirection
+        direction: null,
+      };
+      this.ball.x = this.ball.originalX;
+      this.ball.y = this.ball.originalY;
+      this.ball.speed = this.ball.originalSpeed;
+      this.ball.direction = this.ball.originalDirection;
+  }
 
+  create() {
     let keys = {};
-
-    function draw() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.fillStyle = '#000000'
-        context.fillRect(0, 0, canvas.width, canvas.height);
-
-        context.fillStyle = '#ffffff';
-        context.fillRect(player_1.x, player_1.y, player_1.width, player_1.height);
-
-        context.fillStyle = '#ffffff';
-        context.fillRect(player_2.x, player_2.y, player_2.width, player_2.height);
-
-        context.beginPath()
-        context.setLineDash([10, 15])
-        context.moveTo(canvas.width / 2, 10)
-        context.lineTo(canvas.width / 2, canvas.height)
-        context.lineWidth = 10
-        context.strokeStyle = '#ffffff'
-        context.stroke()
-
-        context.font = '100px Arial';
-        context.fillText(`${formatNumber(player_1.score)}`, 50, 100);
-
-        context.font = '100px Arial';
-        context.fillText(`${formatNumber(player_2.score)}`, canvas.width - 150, 100);
-
-        if (gameStarting) {
-            context.font = '50px Arial';
-            context.fillText('Press any key to begin', 125, canvas.height / 2);
-        } else {
-            context.fillStyle = '#ffffff';
-            context.fillRect(ball.x, ball.y, 10, 10)
-        }
-    }
-
     document.addEventListener('keydown', (event) => {
-        let key = event.key
-        keys[key] = true;
+      let key = event.key;
+      keys[key] = true;
 
-        if (gameStarting) {
-            let ballMovement = setInterval(() => {
-                if (ball.direction < 0) {
-                    ball.x += ball.speed * Math.cos(degreesToRadians(ball.direction));
-                    ball.y += ball.speed * Math.sin(degreesToRadians(ball.direction));
-                } else {
-                    ball.x -= ball.speed * Math.cos(degreesToRadians(ball.direction));
-                    ball.y -= ball.speed * Math.sin(degreesToRadians(ball.direction));
-                }
+      if (!this.gameInProgress) {
+        function resetGame(game) {
+          game.paddle1Data = {
+            score: 0,
+            x: 10,
+            y: canvas.height / 2 - 75,
+            width: null,
+            height: null,
+          };
+          game.paddle1Data.width = game.paddleWidth;
+          game.paddle1Data.height = game.paddleHeight;
 
-                if (ball.y < 0 || ball.y > canvas.height) {
-                    if (ball.direction > 0) {
-                        ball.direction += 135
-                        // ball.direction += 90
-                    } else {
-                        ball.direction -= 135
-                        // ball.direction -= 90
-                    }
-                    ball.speed += 1
-                }
-
-                if (ball.x <= player_1.x + player_1.width && ball.y >= player_1.y && ball.y <= player_1.y + player_1.height) {
-                    ball.direction = -ball.direction
-                    if (Math.floor(Math.random() * 2) + 1 === 1) {
-                        ball.direction += 45
-                    } else {
-                        ball.direction -= 45
-                    }
-                    ball.speed += 1
-                }
-
-                if (ball.x >= player_2.x && ball.y >= player_2.y && ball.y <= player_2.y + player_2.height) {
-                    ball.direction = -ball.direction
-                    if (Math.floor(Math.random() * 2) + 1 === 1) {
-                        ball.direction += 45
-                    } else {
-                        ball.direction -= 45
-                    }
-                    ball.speed += 1
-                }
-
-                if (ball.x <= 0) {
-                    ball.x = ball.originalX
-                    ball.y = ball.originalY
-                    player_2.score += 1
-                    ball.x = ball.originalX
-                    ball.y = ball.originalY
-                    ball.speed = ball.originalSpeed
-                    ball.direction = ball.originalDirection
-                } else if (ball.x >= 750) {
-                    ball.x = ball.originalX
-                    ball.y = ball.originalY
-                    player_1.score += 1
-                    ball.x = ball.originalX
-                    ball.y = ball.originalY
-                    ball.speed = ball.originalSpeed
-                    ball.direction = ball.originalDirection
-                }
-
-                window.requestAnimationFrame(draw);
-                if (player_1.score >= 10 || player_2.score >= 10) {
-                    ball.x = -500
-                    context.font = '50px Arial';
-                    console.log(`Player ${player_1.score >= 10 ? 1 : 2} won!`, 125, canvas.height / 2)
-                    context.fillText(`Player ${player_1.score >= 10 ? 1 : 2} won!`, 125, canvas.height / 2);
-                    window.requestAnimationFrame(draw);
-                    clearInterval(ballMovement);
-                }
-                // ball.x = ball.originalX
-            }, 1)
-
-            gameStarting = false
-        } else {
-            if (key === 'w') {
-                if (player_1.y <= 0) return;
-                player_1.y -= 20
-            } else if (key === 's') {
-                if (player_1.y >= 660) return;
-                player_1.y += 20
-            } else if (key === 'ArrowUp') {
-                if (player_2.y <= 0) return;
-                player_2.y -= 20
-            } else if (key === 'ArrowDown') {
-                if (player_2.y >= 660) return;
-                player_2.y += 20
-            }
-            // console.log(`Player 1: ${player_1.y}`)
-            // console.log(`Player 2: ${player_2.y}`)
+          game.paddle2Data = {
+            score: 0,
+            x: canvas.width - 20,
+            y: canvas.height / 2 - 75,
+            width: null,
+            height: null,
+          };
+          game.paddle2Data.width = game.paddleWidth;
+          game.paddle2Data.height = game.paddleHeight;
         }
-    })
+        resetGame(this);
+        this.gameInProgress = true;
+      }
+    });
 
     window.addEventListener('keyup', (event) => {
-        let key = event.key
-        delete keys[key];
+      let key = event.key;
+      delete keys[key];
     });
 
-    function render() {
-        requestAnimationFrame(render);
+    canvas.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+    });
+
+    setInterval(() => {
+      //   window.requestAnimationFrame(this.draw);
+      this.draw();
+      if (this.gameInProgress) {
+        if (keys['w']) {
+          if (this.paddle1Data.y <= 0) return;
+          this.paddle1Data.y -= this.paddleSpeed;
+        }
+        if (keys['s']) {
+          if (this.paddle1Data.y >= 660) return;
+          // if (this.paddle1Data.y >= canvas.width - this.paddleWidth) return;
+          this.paddle1Data.y += this.paddleSpeed;
+        }
+        if (keys['ArrowUp']) {
+          if (this.paddle2Data.y <= 0) return;
+          this.paddle2Data.y -= this.paddleSpeed;
+        }
+        if (keys['ArrowDown']) {
+          if (this.paddle2Data.y >= 660) return;
+          // if (this.paddle2Data.y >= canvas.width - this.paddleWidth) return;
+          this.paddle2Data.y += this.paddleSpeed;
+        }
+
+        if (this.ball.direction < 0) {
+          this.ball.x += Math.cos(degreesToRadians(this.ball.direction)) * this.ball.speed;
+          this.ball.y += Math.sin(degreesToRadians(this.ball.direction)) * this.ball.speed;
+        } else {
+          this.ball.x -= Math.cos(degreesToRadians(this.ball.direction)) * this.ball.speed;
+          this.ball.y -= Math.sin(degreesToRadians(this.ball.direction)) * this.ball.speed;
+        }
+
+        if (this.ball.y <= 0 || this.ball.y >= canvas.height) {
+          if (this.ball.direction > 0) {
+            this.ball.direction += 90;
+          } else {
+            this.ball.direction -= 90;
+          }
+          this.ball.speed += 1;
+        }
+
+        if (this.ball.x <= this.paddle1Data.x + this.paddle1Data.width && this.ball.y >= this.paddle1Data.y && this.ball.y <= this.paddle1Data.y + this.paddle1Data.height) {
+          this.ball.direction = -this.ball.direction;
+          if (Math.floor(Math.random() * 2) + 1 === 1) {
+            this.ball.direction += 45;
+          } else {
+            this.ball.direction -= 45;
+          }
+          this.ball.speed += 1;
+        }
+
+        if (this.ball.x >= this.paddle2Data.x && this.ball.y >= this.paddle2Data.y && this.ball.y <= this.paddle2Data.y + this.paddle2Data.height) {
+          this.ball.direction = -this.ball.direction;
+          if (Math.floor(Math.random() * 2) + 1 === 1) {
+            this.ball.direction += 45;
+          } else {
+            this.ball.direction -= 45;
+          }
+          this.ball.speed += 1;
+        }
+
+        if (this.ball.x <= 0 || this.ball.x >= canvas.width) {
+          function resetBall(ball) {
+            ball.x = ball.originalX;
+            ball.y = ball.originalY;
+            ball.x = ball.originalX;
+            ball.y = ball.originalY;
+            ball.speed = ball.originalSpeed;
+            ball.direction = ball.originalDirection;
+          }
+          resetBall(this.ball);
+          console.log(this.ball.x)
+
+          if (this.ball.x <= 0) {
+            this.paddle2Data.score += 1;
+          } else {
+            this.paddle1Data.score += 1;
+          }
+          console.log(this.paddle1Data, this.paddle2Data);
+        }
+
+        if ([this.paddle1Data.score, this.paddle2Data.score].some((value) => value >= this.scoreLimit)) {
+          this.ball.x = -500;
+          context.font = '50px Arial';
+          context.fillText(`Player ${this.paddle1Data.score >= this.scoreLimit ? 1 : 2} won!`, 125, canvas.height / 2);
+          setTimeout(() => {
+            console.log(1)
+            this.gameInProgress = false;
+          });
+        }
+      }
+    }, 1);
+  }
+
+  draw() {
+    function drawCanvas() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.fillStyle = '#000000';
+      context.fillRect(0, 0, canvas.width, canvas.height);
     }
 
-    render()
+    function drawPlayerScores(paddle1Data, paddle2Data) {
+      context.fillStyle = '#ffffff';
+      context.fillRect(paddle1Data.x, paddle1Data.y, paddle1Data.width, paddle1Data.height);
+      context.font = '100px Arial';
+      context.fillText(`${formatNumber(paddle1Data.score)}`, 50, 100);
 
-    game.addEventListener('contextmenu', (event) => {
-        event.preventDefault()
-    });
+      context.fillStyle = '#ffffff';
+      context.fillRect(paddle2Data.x, paddle2Data.y, paddle2Data.width, paddle2Data.height);
+      context.font = '100px Arial';
+      context.fillText(`${formatNumber(paddle2Data.score)}`, canvas.width - 150, 100);
+    }
 
-    draw();
+    function drawDashedLine() {
+      context.beginPath();
+      context.setLineDash([10, 15]);
+      context.moveTo(canvas.width / 2, 10);
+      context.lineTo(canvas.width / 2, canvas.height);
+      context.lineWidth = 10;
+      context.strokeStyle = '#ffffff';
+      context.stroke();
+    }
+
+    function drawStartingMessage() {
+      context.font = '50px Arial';
+      context.fillText('Press any key to begin', 125, canvas.height / 2);
+    }
+
+    function drawBall(x, y, width, height) {
+      context.fillStyle = '#ffffff';
+      context.fillRect(x, y, width, height);
+    }
+
+    drawCanvas();
+    drawPlayerScores(this.paddle1Data, this.paddle2Data);
+    drawDashedLine();
+
+    if (!this.gameInProgress) {
+      drawStartingMessage();
+      return;
+    }
+    drawBall(this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+  }
 }
 
-main()
+function normaliseAngle(angle) {
+  let newAngle = angle % 360;
+  if (newAngle < 0) {
+    newAngle += 360;
+  }
+  return newAngle;
+}
+
+function angleCalculator(x1, y1, x2, y2) {
+  let differenceInX = getDifference(x1, x2);
+  let differenceInY = getDifference(y1, y2);
+
+  let opposite = differenceInY;
+  let adjacent = differenceInX;
+
+  let radianAngle = Math.atan(degreesToRadians(opposite / adjacent));
+  let degreesAngle = radiansToDegrees(radianAngle);
+
+  return degreesAngle;
+}
+
+function formatNumber(number) {
+  return number.toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+}
+
+function degreesToRadians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+function radiansToDegrees(radians) {
+  return radians * (180 / Math.PI);
+}
+
+function getDifference(a, b) {
+  return a > b ? a - b : b - a;
+}
+
+let game = new Game();
+game.create();
